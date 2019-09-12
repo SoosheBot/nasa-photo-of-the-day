138f2136fec604478248c95269981145eb814277
sooshe <sooshe@sooshebot.com>
2019-09-12 13:24:02 -0500

    refactored nasaPOD for DRY functionality

--- src/components/NasaCard.js
+++ src/components/NasaCard.js
@@ -1,13 +1,16 @@
 import React from "react";
 
 const NasaCard = props => {
+    console.log("the props", props);
     return (
-        <div className="info-list">
+        <div className="info-list" key={props.data}>
             <h2>Title: {props.title}</h2>
-            <h4>Copyright: {props.copyright}</h4>
-            <h3>Date: {props.date}</h3>
-            <p>{props.explanation}</p>
-            <img className="hd-img" alt="NASA images" src={props.hdUrl} />
+            <img className="imgURL" alt="NASA images" src={props.hdurl}></img>
+            <p><strong>About this image:</strong> {props.explanation}</p>
+            <h5>Copyright: {props.copyright}</h5>
+            <h5>Date: {props.date}</h5>
+            
+            
         </div>
       );
 };
--- src/components/NasaPOD.js
+++ src/components/NasaPOD.js
@@ -1,30 +1,20 @@
 import React, { useState, useEffect } from "react";
-import NasaCard from "/Users/Mahadevi/Lambda-School/nasa-photo-of-the-day/src/components/NasaCard.js";
+import NasaCard from "./NasaCard";
 import axios from "axios";
-// import { thisExpression } from "@babel/types";
+
 
 
 
 
 export default function NasaPOD() {
-    const [title, setTitle] = useState('');
-    const [copy, setCopy] = useState('');
-    const [date, setDate] = useState('');
-    const [desc, setDesc] = useState('');
-    const [headURL, setHDUrl] = useState('');
+  const [nasaPic, setNasaPics] = useState("");
 
     useEffect(() => {
         axios
         .get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
         .then(response => {
-            setTitle(response.data.title);
-            setCopy(response.data.copyright);
-            setDate(response.data.date);
-            setDesc(response.data.explanation);
-            setHDUrl(response.data.hdurl);
-            
-        
-            console.log("THE CONSOLE", response);
+            setNasaPics(response.data) 
+            console.log("NASA", response);
         })
         .catch(error => {
             console.log("That's not the sky you're seeing!", error);
@@ -34,11 +24,12 @@ export default function NasaPOD() {
     return (
         <div className="info">
               <NasaCard
-                title={title.title}
-                copyright={copy.copyright}
-                date={date.date}
-                description={desc.explanation}
-                headUrl={headURL.hdurl}
+                key={nasaPic.data}
+                title={nasaPic.title}
+                imgUrl={nasaPic.hdurl}
+                description={nasaPic.explanation}
+                date={nasaPic.date}
+                copyright={nasaPic.copyright}   
               />
           
         </div>
